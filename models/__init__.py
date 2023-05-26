69851b48b5c4f73d4ceafe426dafd3a84c149d0f
sfl-eso <sfl-eso@opa-galaxy.space>
2023-05-26 18:46:10 +0200

    remove old ability uptime model

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,3 @@ from .ability_events import AbilityInfo, EffectInfo, BeginCast, EndCast, EffectC
 from .unit_events import PlayerInfo, UnitAdded, UnitRemoved, UnitChanged
 from .meta_events import BeginLog, EndLog, BeginCombat, EndCombat, ZoneChanged, MapChanged, TrialInit
 from .log import EncounterLog
-from .ability_uptime import AbilityUptime
